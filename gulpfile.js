4846817eaf60d4f562e58753d84f58af4dcfffee
Antonin Januska <antonin@antjanus.com>
2014-07-24 20:15:48 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+
+gulp.task('scripts', function() {
+  gulp.src(['./js/dev/*.js'])
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/src/'))
+  ;
+});
+
+gulp.task('styles', function() {
+  gulp.src(['style.less', 'tiny-mce-style.less'])
+    .pipe(autoprefix())
+    .pipe(less())
+    .pipe(gulp.dest('./'))
+  ;
+});
+
+gulp.task('default', ['scripts', 'styles'], function(){
+  gulp.watch('./js/dev/*.js', ['jshint', 'scripts']);
+  gulp.watch('./less/*.less', ['styles']);
+});
